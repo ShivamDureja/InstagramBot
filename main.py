565de8cb9827cf7ca2aa5a79fc5de3d0c0d42a60
Shivam Dureja <shivamdureja13@gmail.com>
2022-07-05 12:26:39 +0530

    login button configured

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Loader import Loader
 from Login import LoginUser
-
+from LoginButton import submit
 
 
 page_url = "https://www.instagram.com/"
@@ -8,3 +8,4 @@ login_name = "_shivam_dureja"
 login_password = "sdshivam"
 driver = Loader(page_url)
 LoginUser(driver,login_name,login_password)
+submit(driver)

9c202873968f82aa34d8cfa250e5e0a76b706012
Shivam Dureja <shivamdureja13@gmail.com>
2022-07-05 12:33:22 +0530

    login remember popup is configured

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from Loader import Loader
 from Login import LoginUser
 from LoginButton import submit
+from removePopup import popup
+import time
+
 
 
 page_url = "https://www.instagram.com/"
 login_name = "_shivam_dureja"
 login_password = "sdshivam"
 driver = Loader(page_url)
+time.sleep(2)
 LoginUser(driver,login_name,login_password)
+time.sleep(2)
 submit(driver)
+time.sleep(2)
+popup(driver)

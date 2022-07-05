1cbb84a51c97f0976be5613c30bf3b0681bb45e5
Shivam Dureja <shivamdureja13@gmail.com>
2022-07-05 12:13:01 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from Loader import Loader
+from Login import LoginUser
+
+
+
+page_url = "https://www.instagram.com/"
+login_name = "_shivam_dureja"
+login_password = "sdshivam"
+driver = Loader(page_url)
+LoginUser(driver,login_name,login_password)
